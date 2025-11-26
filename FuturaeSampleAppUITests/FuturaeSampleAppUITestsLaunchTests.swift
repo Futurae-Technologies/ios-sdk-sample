c6dc4102d04f377bb8284e7a67f85cacec678b40
dimitris Tsigouris <dimitris@futurae.com>
2025-11-26 12:56:22 +0200

    chore: add FuturaeSampleAppUITests.xcscheme and add sanity check ui test

---
+++ FuturaeSampleAppUITests/FuturaeSampleAppUITestsLaunchTests.swift
@@ -0,0 +1,64 @@
+//
+//  FuturaeSampleAppUITestsLaunchTests.swift
+//  FuturaeSampleAppUITests
+//
+//  Created by Dimitrios Tsigouris on 11/25/25.
+//
+import XCTest
+
+final class FuturaeSampleAppUITestsLaunchTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunchSDKAndNavigation() throws {
+        let app = XCUIApplication()
+        
+        // Interruption monitor
+        addUIInterruptionMonitor(withDescription: "System Alerts") { alert in
+            if alert.buttons["Allow"].exists {
+                alert.buttons["Allow"].tap()
+                return true
+            }
+            if alert.buttons["Allow While Using App"].exists {
+                alert.buttons["Allow While Using App"].tap()
+                return true
+            }
+            return false
+        }
+        
+        app.launch()
+        app.tap()   
+        
+        app.swipeUp()
+        app.swipeUp()
+        app.swipeUp()
+        
+        let launchButton = app.buttons["Launch SDK"]
+        
+        let exists = NSPredicate(format: "exists == true && isHittable == true")
+        expectation(for: exists, evaluatedWith: launchButton)
+        waitForExpectations(timeout: 5)
+        
+        launchButton.tap()
+        
+        let moreButton = app.tabBars.buttons.matching(identifier: "More").firstMatch
+        XCTAssertTrue(moreButton.exists)
+        moreButton.tap()
+        
+        let settingsCell = app.staticTexts["Settings"]
+        XCTAssertTrue(settingsCell.exists)
+        settingsCell.tap()
+        
+        let sdkFunctions = app.staticTexts["SDK Functions"]
+        XCTAssertTrue(sdkFunctions.exists)
+        sdkFunctions.tap()
+        
+        let jbStatus = app.buttons["Check Jailbreak Status"]
+        XCTAssertTrue(jbStatus.exists)
+        jbStatus.tap()
+    }
+
+}
